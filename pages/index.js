05a8ab1d8cbd1108e08931f6b3108e572e2cd10c
wonnie1201 <wodnjs7030@naver.com>
2025-03-02 22:32:06 +0900

    Update files with latest changes

--- pages/index.js
+++ pages/index.js
@@ -306,7 +306,7 @@ export default function Home() {
               id="fileInput"
               type="file"
               accept="image/*"
-              onChange={handleImageUpload}
+              onChange={handleFileInput}
               style={{ display: 'none' }}
             />
           </div>
